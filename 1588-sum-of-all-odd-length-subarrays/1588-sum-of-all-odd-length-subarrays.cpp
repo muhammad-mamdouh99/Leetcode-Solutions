7e5d75bfb926f432be7dee53280bbb37d4d52edb
Muhammad Mamdouh <muhammad.mamdouh99@gmail.com>
2022-09-10 14:10:08 +0200

    Time: 3 ms (81.24%), Space: 8.4 MB (38.28%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -4,9 +4,9 @@ public:
         int sum = 0, len = arr.size();
         for (int i = 0; i < len; ++i) {
             int left = i + 1, right = len - i;
-            int all_occur = left * right;
-            int odd_occur = all_occur / 2 + (all_occur % 2);
-            sum += odd_occur * arr[i];
+            int all_sub = left * right;
+            int odd_sub = all_sub / 2 + (all_sub % 2);
+            sum += odd_sub * arr[i];
         }
         return sum;
     }
